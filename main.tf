27ef621800e391dabc2b0b1320c4ad2a6f50d53c
Cyprian Gepfert <cgepfert@gmail.com>
2019-05-26 22:10:16 +0200

    Add tf security group

--- main.tf
+++ main.tf
@@ -1,8 +1,33 @@
 provider "aws" {
-  region= "eu-central-1"
+  region = "eu-central-1"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-8504fdea"
+  ami           = "ami-8504fdea"
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.instance.id}",
+  ]
+
+  tags {
+    Name = "terraform-example"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
